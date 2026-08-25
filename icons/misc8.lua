f80a45a074cd353e5f1677fbfc89dab4fc11b859
Soul_Hagans <46518893+SoulHagans@users.noreply.github.com>
2026-08-25 20:32:33 +0600

    Update misc8.lua for @Soul_Hagans

--- icons/misc8.lua
+++ icons/misc8.lua
@@ -7,7 +7,7 @@ local a = lib.GetAnimatedTable()
 
 s["@RedBoul"] = "LibCustomIcons/icons/misc8/RedBoul.dds"
 s["@Beejy"] = "LibCustomIcons/icons/misc8/Beejy.dds"
-s["@Soul_Hagans"] = "LibCustomIcons/icons/misc8/Soul_Hagans.dds"
+a["@Soul_Hagans"] = {"LibCustomIcons/icons/misc8/Soul_Hagans_anim.dds", 13, 1, 10}
 s["@Ukmadorian"] = "LibCustomIcons/icons/misc8/Ukmadorian.dds"
 a["@Kuroko29012"] = {"LibCustomIcons/icons/misc8/Kuroko29012_anim.dds", 17, 1, 10}
 s["@Chio-Cill"] = "LibCustomIcons/icons/misc8/Chio_Cill.dds"
